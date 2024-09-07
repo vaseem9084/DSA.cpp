67588e15e523d5203b366afc105b2a0477b0f8a4
VASEEM <92212839+vaseem9084@users.noreply.github.com>
2024-09-07 21:44:36 +0530

    Add files via upload

---
+++ Reverse-stack.cpp
@@ -0,0 +1,23 @@
+// Reverse stack using string
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+int main()
+{
+    string str = "my_name_vaseem";
+    stack<char> s;
+    for(int i = 0; i<str.length();i++){
+        char ch = str[i];
+        s.push(ch);
+
+    }
+    string ans = "";
+    while (!s.empty()){
+        char ch = s.top();
+        ans.push_back(ch);
+        s.pop();
+    }
+    cout<<"answer is: "<<ans<<endl;
+}
